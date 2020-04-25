3d57492628bae69a9281354a2f93c90a37e53749
Eduardo Rojas Oviedo <50854988+erojaso@users.noreply.github.com>
2020-04-24 19:33:22 -0600

    Project structure
    Structure changes

---
+++ Time-Series-Resources/TimeSeries_Principles_and_Practice/src/__init__.py
@@ -0,0 +1,15 @@
+__all__ = ['00_utils', 
+            '01_data',
+            '02_intermediate',
+            '03_processing',
+            '04_modelling',
+            '05_model_evaluation',
+            '06_reporting',
+            '06_visualisation']
+
+#from src.00_utils import foo
+
+if __name__ == '__main__':
+    print('main')
+else:
+    print(_name__)
